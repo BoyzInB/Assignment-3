b92a05d705e60064e40d7971295ba8b7b010dd40
Robin Eriksson <robineriksson.mail@gmail.com>
2016-03-01 13:45:43 +0100

    working?

--- poisson.c
+++ poisson.c
@@ -7,41 +7,41 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-int n;
 
-void printArray(int A[][]){
+
+void printArray(const int *A,int n){
     for (int i = 0; i<n; i++){
         for(int j=0;j<n;j++)
-            printf("%d",A[i][j]);
+            printf("%d",A[i*n+j]);
         printf("\n");
     }
 }
-   
 
 
 
 int main(int argc, char *argv[]) {
     
     int i,j,n = 10;
-    int *A;
     
-    //A = (int **)malloc(n*sizeof(int *));
+    int *A;
     A = (int *) malloc(n*n*sizeof(int *));
     
     
     for (i = 0; i<n; i++){
         for(j=0;j<n;j++){
             if(i == 0 || i == n-1)
-                A[i][j] = 0;
+                A[i*n+j] = 0;
             else if(j == 0 || j == n-1)
-                A[i][j] = 0;
+                A[i*n+j] = 0;
             else
-                A[i][j] = 1;
+                A[i*n+j] = 1;
         }
     }
-    A[3][4] = 5;
+    A[(3-1)*n+(4-1)] = 5;
     
-    printArray(A);
+   
+    
+    printArray(A,n);
   
 
 

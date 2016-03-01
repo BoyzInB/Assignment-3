c10a85eed76cae3b9623240397da7b7a070ec032
Robin Eriksson <robineriksson.mail@gmail.com>
2016-03-01 12:42:32 +0100

    hello world

--- poisson.c
+++ poisson.c
@@ -1,1 +1,13 @@
-//hello world
+/**********************************************************************
+ * assignment 3
+ *
+ *
+ **********************************************************************/
+
+#include <omp.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("hello world!");
+    return 0;
+}

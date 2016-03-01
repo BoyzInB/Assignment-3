395601cb247b21bb193da42895d08a2a894c2ab8
Robin Eriksson <robineriksson.mail@gmail.com>
2016-03-01 14:54:55 +0100

    new

--- poisson.c
+++ poisson.c
@@ -1,74 +1,163 @@
-/**********************************************************************
- * assignment 3
+/*
+ *				solvers: main.c
  *
+ *	This program solves Laplace's equation on the unit square in
+ *	finite-difference form using Jacobi, Gauss-Seidel, or SOR iteration,
+ *	or a full-matrix or banded-matrix direct solve.
  *
- **********************************************************************/
+ *	The BCs are Dirichlet with u(B) = 0 except u(x = 1, y) = 1.
+ *
+ *	In u(x_i,y_j) = u_ij = u[j][i], x[i] runs from xmin to xmax & y[j] runs
+ *	from ymin to ymax:
+ *		x = xmin + i*dx;
+ *		y = ymin + j*dy;
+ */
 
-#include <omp.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define PI 3.14159265
+#define MAX_SWEEPS	1000000
 
+#define PI	3.142
+#define DOUBLE		((unsigned) sizeof(double))
+#define ERROR	(-1)
+#define sq(x)	( (x)*(x) )
+#define EPSILON_M	2.2204460492503131e-16
 
-void printArray(const double *A,int n){
-    //Print array
-    for (int i = 0; i<n; i++){
-        for(int j=0;j<n;j++)
-            printf("%f  ",A[i*n+j]);
+typedef struct {
+    int N;
+    double dx, xmin, xmax;
+    double dy, ymin, ymax;
+    double h;
+    double omega, EPSILON;
+} GRID;
+
+typedef void	*POINTER;
+
+void implement_BCs(GRID *grid, double *u[]);
+void print_solution(char *string, GRID *grid, double *u[]);
+void SOR(GRID *grid, double *u[]);
+
+POINTER alloc_matrix(int N_rows, int N_columns, unsigned element_size);
+POINTER alloc_vector(int N, unsigned element_size);
+POINTER Alloc(unsigned N_bytes);
+void zero_matrix(double *A[], int N_rows, int N_columns);
+void zero_vector(double v[], int N);
+
+int main(void)
+{
+    GRID grid;
+    double xmin = 0., xmax = 1.;
+    double ymin = 0., ymax = 1.;
+    double h, mu;
+    /* for convergence of iterative methods, EPSILON = epsilon*h^2 */
+    double epsilon = 1.e-5;
+    int N,i;
+    void *elliptic;
+    char c[100];
+    
+    N = 10;
+    printf("number of dx = number of dy = %d\n", N);
+    grid.N = N;
+    grid.dx = (xmax-xmin)/N;
+    grid.dy = (ymax-ymin)/N;
+    h = grid.h = grid.dx;
+    grid.xmin = xmin;
+    grid.xmax = xmax;
+    grid.ymin = ymin;
+    grid.ymax = ymax;
+    grid.EPSILON = epsilon*sq(h);
+    
+    elliptic = SOR;
+    /* calculate omega_opt for SOR */
+    //mu = cos(PI*h); /* Jacobi spectral radius */
+    grid.omega = 1.7;
+    printf("EPSILON = epsilon*h^2 = %g\n", grid.EPSILON);
+    printf("\twhere epsilon = %g\n", epsilon);
+    printf("SOR omega_opt = %g\n", grid.omega);
+    
+    /* allocate memory for array u */
+    double **u = calloc((N+1),sizeof(*u));
+    double *arr = calloc((N+1)*(N+1), sizeof*arr);
+    
+    for (i = 0; i < (N+1); ++i)
+    {
+        u[i] = &arr[i * (N+1)];
+    }
+    
+    implement_BCs(&grid, u);
+    SOR(&grid, u);
+    print_solution("solution", &grid, u);
+    
+    free(arr);
+    free(u);
+    return 0;
+}
+
+void implement_BCs(GRID *grid, double *u[])
+{
+    int N = grid->N, i, j;
+    
+    for (j = 0; j <= N; j++) {
+        u[j][0] = 0.;
+        u[j][N] = 1.;
+    }
+    for (i = 1; i < N; i++) {
+        u[0][i] = 0.;
+        u[N][i] = 0.;
+    }
+}
+
+void print_solution(char *string, GRID *grid, double *u[])
+{
+    int N = grid->N, i, j;
+    double x, y;
+    
+    // x, y, u
+    for (i = 0; i <= N; i++) {
+        for (j = 0; j <= N; j++) {
+            printf("%.2f ", u[j][i]);
+        }
         printf("\n");
     }
+    
 }
 
-double sinfunc (double x, double y){
-    double res = sin(2*PI*x);
-    return res;
-}
-
-int main(int argc, char *argv[]) {
+void SOR(GRID *grid, double *u[])
+{
+    int N = grid->N, i, j, sweep;
+    double EPSILON = grid->EPSILON;
+    double omega = grid->omega;
+    double sum, residual, norm_residual0, norm_residual = 1.;
     
-    int i,j,n = 10;
-    
-    double *A,*f;
-    
-    //initialize the array
-    A = (double *) malloc(n*n*sizeof(double *));
-    f = (double *) malloc(n*n*sizeof(double *));
-
-    
-    //initial values ...
-    for (i = 0; i<n; i++){
-        for(j=0;j<n;j++){
-            // fill boundaries
-            if(i == 0 || i == n-1)
-                A[i*n+j] = 0.0;
-            else if(j == 0 || j == n-1)
-                A[i*n+j] = 0.0;
-            // fill inner
-            else
-                A[i*n+j] = 1.0;
+    sum = 0;
+    for (i = 1; i < N; i++) {
+        for (j = 1; j < N; j++) {
+            residual = (-4.*u[j][i]+u[j+1][i]+u[j-1][i]+
+                        u[j][i+1]+u[j][i-1]);
+            sum += fabs(residual);
         }
     }
+    norm_residual0 = sum/sq(N-1);
     
-    //f-function
-    for (i = 0; i<n; i++){
-        for(j=0;j<n;j++){
-            f[i*n+j] = sinfunc(i,j);
+    for (sweep = 0; sweep<MAX_SWEEPS &&
+         norm_residual>EPSILON*norm_residual0; sweep++) {
+        sum = 0;
+        for (i = 1; i < N; i++) {
+            for (j = 1; j < N; j++) {
+                residual = (-4.*u[j][i]+u[j+1][i]+u[j-1][i]+
+                            u[j][i+1]+u[j][i-1]);
+                sum += fabs(residual);
+                u[j][i] += omega*residual/4.;
+            }
         }
+        norm_residual = sum/sq(N-1);
     }
-    //Extra value
-    A[(3-1)*n+(4-1)] = 5.0;
-    
-   
-    //Print array
-    printArray(A,n);
-    printf("\n");
-    printArray(f,n);
-  
-    free(A);
-    free(f);
-
-return 0;
+    printf("number of sweeps = %d\n", sweep);
+    printf("norm_residual/norm_residual0 = %g\n",
+           norm_residual/norm_residual0);
+    if (norm_residual > EPSILON*norm_residual0)
+        printf("WARNING: iterative method failed to converge\n");
 }
-

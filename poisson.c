e56aef171c951d9429bc898686f1cc40410b0f4b
Robin Eriksson <robineriksson.mail@gmail.com>
2016-03-07 17:06:20 +0100

    inget funkar för fan ...

--- poisson.c
+++ poisson.c
@@ -22,6 +22,7 @@
 
 typedef struct {
     int N;
+    int Nx,Ny;
     double dx, i_max;
     double dy, j_max;
     double h;
@@ -49,14 +50,18 @@ int main(void)
     /* for convergence of iterative methods, EPSILON = epsilon*h^2 */
     double epsilon = 1.e-5;
     int N,i;
-    char c[100];
+    int Nx, Ny;
     
-    N = 100;
+    N = 10;
+    grid.Nx = 10;
+    grid.Ny = 10;
     grid.N = N;
     int nTemp = N - (N%2 == 0);
+    int nxTemp = Nx - (Nx%2 == 0);
+    int nyTemp = Ny - (Ny%2 == 0);
     double a=1.,b=1.;
-    grid.i_max = N - 2;
-    grid.j_max = N - 2;
+    grid.i_max = Nx - 2;
+    grid.j_max = Ny - 2;
     grid.dx = a/grid.i_max;
     grid.dy = b/grid.j_max;
     
@@ -70,20 +75,25 @@ int main(void)
     printf("\twhere epsilon = %g\n", epsilon);
     printf("SOR omega_opt = %g\n", grid.omega);
     
+    printf("hej\n");
+
     /* allocate memory for array u */
-    double **u = calloc((N),sizeof(*u));
-    double *arr1 = calloc((N)*(N), sizeof*arr1);
-    double **f = calloc((N),sizeof(*f));
-    double *arr2 = calloc((N)*(N), sizeof*arr2);
+    double **u = calloc(Ny,sizeof(*u));
+    double *arr1[Ny] = calloc(Nx, sizeof*arr1);
+    double **f = calloc(Ny,sizeof(*f));
+    double *arr2 = calloc(Nx, sizeof*arr2);
     
-    
-    for (i = 0; i < (N); ++i)
+    printf("\thej\n");
+
+    for (i = 0; i < (Nx); ++i)
     {
         u[i] = &arr1[i * (N)];
-        f[i] = &arr2[i * (N)];
+        f[i] = &arr2[i * (Ny)];
     }
+    printf("\t\thej\n");
+
     
-    fillF(&grid,f);
+    //fillF(&grid,f);
     
     //print_solution("f",&grid,f);
     
@@ -115,10 +125,13 @@ void fillF(GRID *grid, double *f[]){
     double dx = grid->dx;
     double dy = grid->dy;
     int N = grid->N;
+    int Nx = grid->Nx;
+    int Ny = grid->Ny;
+
     int i,j;
     
-    for (i = 0; i<N; i++)
-        for(j = 0;j<N;j++){
+    for (i = 0; i<Nx; i++)
+        for(j = 0;j<Ny;j++){
             f[i][j] = sin(2*PI*(i-0.5)*dx);
             // printf("x: %f\n",(i-0.5)*dx);
         }
@@ -131,28 +144,40 @@ void fillF(GRID *grid, double *f[]){
 void implement_BCs(GRID *grid, double *u[])
 {
     int N = grid->N, i, j;
-    
-    for (i = 1; i < N-1 ; i++) {
+    int Nx = grid->Nx;
+    int Ny = grid->Ny;
+
+
+    printf("hej 1\n");
+
+    for (i = 1; i < Nx-1 ; i++) {
         u[0][i] = u[1][i];
-        u[N-1][i] = u[N-2][i];
+
+        u[Ny-1][i] = u[Ny-2][i];
     }
-    for (j = 1; j < N-1; j++) {
+    printf("\thej 2\n");
+
+    for (j = 1; j < Ny-1; j++) {
         u[j][0] = u[j][1];
-        u[j][N-1] = u[j][N-2];
+        u[j][Nx-1] = u[j][Nx-2];
     }
-    u[0][0] = u[1][1];u[N-1][0]=u[N-2][1];
-    u[0][N-1] = u[1][N-2];u[N-1][N-1]=u[N-2][N-2];
-}
+    u[0][0] = u[1][1];u[Ny-1][0]=u[Ny-2][1];
+    u[0][Nx-1] = u[1][Nx-2];u[Ny-1][Nx-1]=u[Ny-2][Nx-2];
+
+     }
 
 
 void print_solution(char *string, GRID *grid, double *u[])
 {
     int N = grid->N, i, j;
+    int Nx = grid->Nx;
+    int Ny = grid->Ny;
+
     double x, y;
     
     // x, y, u
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N ; j++) {
+    for (i = 0; i < Nx; i++) {
+        for (j = 0; j < Ny ; j++) {
             printf("%.5f ", u[j][i]);
         }
         printf("\n");
@@ -162,7 +187,11 @@ void print_solution(char *string, GRID *grid, double *u[])
 
 void SOR(GRID *grid, double *u[], double *f[])
 {
-    int N = grid->N, i, j, sweep;
+    int N = grid->N, i, j;
+    int Nx = grid->Nx;
+    int Ny = grid->Ny;
+
+
     double dx = grid->dx;
     double dy = grid->dy;
     int i_max = grid->i_max;
@@ -171,39 +200,62 @@ void SOR(GRID *grid, double *u[], double *f[])
     double omega = grid->omega;
     double sum, temp, norm_residual0, norm_residual = 1.;
     
-    double **residual = calloc((N),sizeof(*residual));
-    double *arr3 = calloc((N)*(N), sizeof*arr3);
     
-    for (i = 0; i < (N); ++i)
+    printf("hej 1 \n");
+
+    
+
+    double **residual = calloc(Ny,sizeof(*residual));
+    double *arr3 = calloc(Nx, sizeof*arr3);
+    
+    printf("\thej 2\n");
+
+    
+
+    for (i = 0; i < Ny; ++i)
     {
-        residual[i] = &arr3[i * (N)];
+        residual[i] = &arr3[i * (Nx)];
     }
     
-    
+    printf("\t\thej 3 \n");
+
+  
     int count;
     
     //Step 1: enforce BC
+    printf("\t\t\thej 4 \n");
+
     implement_BCs(grid,u);
+    printf("\t\t\t\thej 5 \n");
 
 #pragma omp parallel
     {
         count = 0;
         while(count < 50000 && norm_residual > EPSILON){
+
 #pragma omp single
             count++;
             //Step 2: compute new u
             
             //Red
+
 #pragma omp for private(j)
-            for (i = 1; i < N-1; i++)
-                    for (j = 1 + (i%2!=0); j < N-1; j+=2)
-                        u[j][i] =(1-omega)*u[j][i] + omega/(2/(dx*dx)+2/(dy*dy))*
-                        ( (u[j+1][i]+u[j-1][i])/(dx*dx)+
+            for (i = 1; i < Nx-1; i++){
+                for (j = 1 + (i%2!=0); j < Ny-1; j+=2){
+
+                    u[j][i] =(1-omega)*u[j][i];
+
+                    u[j][i] += omega/(2/(dx*dx)+2/(dy*dy));
+
+                    u[i][j] *= ( (u[j+1][i]+u[j-1][i])/(dx*dx)+
                          (u[j][i+1]+u[j][i-1])/(dy*dy) -f[j][i]);
+                }
+            }
+            
             //Black
 #pragma omp for private(j)
-            for (i = 1; i < N-1; i++)
-                    for (j = 1 + (i%2==0); j < N-1; j+=2)
+            for (i = 1; i < Nx-1; i++)
+                    for (j = 1 + (i%2==0); j < Ny-1; j+=2)
                         u[j][i] =(1-omega)*u[j][i] + omega/(2/(dx*dx)+2/(dy*dy))*
                         ( (u[j+1][i]+u[j-1][i])/(dx*dx)+
                          (u[j][i+1]+u[j][i-1])/(dy*dy) -f[j][i]);
@@ -215,8 +267,8 @@ void SOR(GRID *grid, double *u[], double *f[])
             
             sum = 0.0;
 #pragma omp for reduction(+:sum) private(j)
-            for (i = 1; i < N-1; i++) {
-                for (j = 1; j < N-1; j++) {
+            for (i = 1; i < Nx-1; i++) {
+                for (j = 1; j < Ny-1; j++) {
                     residual[j][i] = f[j][i]-((u[j+1][i] - 2*u[j][i] + u[j-1][i])/(dx*dx)
                                               + (u[j][i+1] - 2*u[j][i] + u[j][i-1])/(dy*dy));
                     sum = residual[j][i]*residual[j][i];
@@ -225,8 +277,10 @@ void SOR(GRID *grid, double *u[], double *f[])
             //Step 5: compute it's L2norm
 #pragma omp single
             norm_residual = sqrt(1.0/((double)i_max*(double)j_max)*sum);
+            
         }
     }
+
     printf("count: %d\n", count);
     printf("sor done ...\n");
     
